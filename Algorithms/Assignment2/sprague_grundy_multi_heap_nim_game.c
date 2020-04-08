cc2124f2518f21ccbc347b1185669b9a6686b1fd
Siladittya Manna <sadimanna@users.noreply.github.com>
2020-04-08 20:32:46 +0530

    Update sprague_grundy_multi_heap_nim_game.c

--- Algorithms/Assignment2/sprague_grundy_multi_heap_nim_game.c
+++ Algorithms/Assignment2/sprague_grundy_multi_heap_nim_game.c
@@ -1,6 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//CALCULATING MAXIMUM NUMBER OF BIT
+//REQUIRED TO REPRESENT THE NUMBER
+int get_maxlen(int dec)
+{
+	int maxlen = 1;
+	int mult2 = 1;
+	//CALCULATING THE SMALLEST POWER OF 2, THE DECIMAL NUMBER DOES NOT EXCEED
+	while(mult2<dec)
+	{
+		maxlen++;
+		mult2*=2;
+	}
+	if(mult2>dec)
+		maxlen--;
+	//printf("\nFOR %d maximum number of bits required is %d\n",dec,maxlen);
+	return maxlen;
+}
+
+//DECIMAL TO BINARY CONVERSION
+int *dec2bin(int dec, int len)
+{
+	static int *binstr;
+	int maxlen;
+	//CALCULATING MAXIMUM NUMBER OF BITS REQUIRED
+	if (len==0)
+		maxlen = get_maxlen(dec);
+	else
+		maxlen = len;
+
+	//printf("FOR %d maximum number of bits required is %d\n",dec,maxlen);
+	binstr = (int *)malloc(maxlen*sizeof(int));
+
+	int tempdec, i = maxlen-1;
+	tempdec = dec;
+	//CALCULATING BINARY REPRESENTATION OF THE DECIMAL NUMBER
+	while(i>=0)
+	{
+		binstr[i]=tempdec%2;
+		tempdec/=2;
+		i--;
+	}
+
+	return binstr;
+}
+
+//BINARY TO DECIMAL CONVERSION
+int bin2dec(int *binstr, int len)
+{
+	if (len==1)
+		return binstr[0];
+
+	int decnum = 0;
+	int coeff = 1;
+	//CONVERTING THE BINARY STRING TO DECIMAL VALUE
+	for(int i=len-1;i>=0;i--)
+	{
+		decnum+=binstr[i]*coeff;
+		coeff*=2;
+	}
+	return decnum;
+}
+
+//BIT-BY-BIT XOR
+int xorb(int b1, int b2)
+{
+	//SINGLE BIT XOR
+	if (b1==1 && b2==1)
+		return 0;
+	if (b1==1 && b2==0)
+		return 1;
+	if (b1==0 && b2==1)
+		return 1;
+	if (b1==0 && b2==0)
+		return 0;
+}
+
+//CALCULATING XOR OF TWO NUMBERS
+int *xor(int *bin_num1, int *bin_num2, int len)
+{
+	int *xorstr = (int *)malloc(len*sizeof(int));
+	//CALCULATING BIT BY BIT XOR
+	for(int i=len-1;i>=0;i--)
+		xorstr[i] = xorb(bin_num1[i],bin_num2[i]);
+
+	return xorstr;
+}
+
+//CALCULATING NIMSUM
+int nimsum(int *num_arr,int num_num)
+{
+	int *bin_num1, *bin_num2, *xor_num1_num2;
+	int len=0, i=0, templen, decval = num_arr[0];
+	//CALCULATING THE MAXIMUM NUMBER OF BITS REQUIRED
+	for(;i<num_num;i++)
+	{
+		templen = get_maxlen(num_arr[i]);
+		if(templen>len)
+			len=templen;
+	}
+	//CALCULATING THE XOR BETWEEN ALL THE VALUES IN num_arr
+	for(i=1;i<num_num;i++)
+	{
+		bin_num1 = dec2bin(decval,len);
+		bin_num2 = dec2bin(num_arr[i],len);
+		xor_num1_num2 = xor(bin_num1,bin_num2,len);
+
+		decval = bin2dec(xor_num1_num2,len);
+	}
+		
+	return decval;
+}
+
+//CHECK IF ALL HEAPS ARE EMPTY
 int all_zero(int *N, int k)
 {
 	int zero_flag = 1;
@@ -14,6 +127,7 @@ int all_zero(int *N, int k)
 	return zero_flag;
 }
 
+//OPTIMAL MOVE ACCORDING TO SPRAGUE-GRUNDY THEOREM
 int sprague_grundy_optimal_move(int n, int m)
 {
 	int opt_move = 0;
@@ -31,54 +145,69 @@ int sprague_grundy_optimal_move(int n, int m)
 	return opt_move;
 }
 
-int is_in_P_position(int n, int m)
-{
-	if (n % (m+1) == 0)
-		return 1;
-	else
-		return 0; 
-}
-
+//CHECK IF IN P POSITION
 int all_P(int *N, int* M, int k)
 {
-	for(int i = 0; i < k; i++)
-		if(is_in_P_position(N[i],M[i])==0)
-			return 0;
-	return 1;
+	int rems[k];
+
+	for(int i=0;i<k;i++)
+		rems[i] = N[i]%(M[i]+1);
+
+	int nim_sum_sg = nimsum(rems,k);
+
+	if(nim_sum_sg!=0)
+		return nim_sum_sg;
+	else 
+		return 0;
 }
 
 int *sprague_grundy_multi_heap_optimal_move(int *N, int *M, int k)
 {
 	int i,j;
 	static int move[2];
-	int N_min,N_min_ind;
+	int N_max,N_max_ind;
+	int nim_sum_sg;
+	//CALCULATE THE NIMSUM OF THE SG-VALUES
+	nim_sum_sg = all_P(N,M,k);
 
-	if(all_P(N,M,k))
+	if(nim_sum_sg==0)
 	{
 		i = 0;
 		while(N[i]==0)
 			i++;
-		N_min = N[i];
-		N_min_ind = i;
+		N_max = N[i];
+		N_max_ind = i;
 
-		for(j=i+1;i<k;i++)
+		for(j=i+1;j<k;j++)
 		{
-			if(N[j]<N_min && N[j]!=0)
-				N_min_ind=j;
+			if(N[j]>N_max && N[j]!=0)
+			{
+				N_max = N[j];
+				N_max_ind=j;
+			}
 		}
 		
-		move[0] = N_min_ind;
-		move[1] = sprague_grundy_optimal_move(N[N_min_ind],M[N_min_ind]);
+		move[0] = N_max_ind;
+		move[1] = sprague_grundy_optimal_move(N[N_max_ind],M[N_max_ind]);
 
 		return move;
 	}
-	
+
+	//GET THE INDEX OF THE MOST SIGNIFICANT BIT
+	int kth_bit = get_maxlen(nim_sum_sg);
+
+	//CALCULATE THE OPTIMAL MOVE
+	int *binstr;
+
 	for(i = 0; i < k; i++)
 	{
-		if(is_in_P_position(N[i],M[i])==0)
+		binstr = dec2bin(N[i]%(M[i]+1),0);
+
+		if(binstr[kth_bit-1]==1)
 		{
 			move[0] = i;
 			move[1] = sprague_grundy_optimal_move(N[i],M[i]);
+			break;
 		}
 	}
 
@@ -99,18 +228,6 @@ void main()
 	char c;
 	//CLEARING THE LF from input buffer
 	c = getchar();
-
-	/*int *N_copy = (int *)malloc(k*sizeof(int));
-
-	for(i=0;i<k;i++)
-		N_copy[i] = N[i];
-
-	int max_moves = 0;
-	for(i=0;i<k;i++)
-		max_moves+=(M[i]*2);
-
-	int *moves_heaps = (int *)malloc(max_moves*sizeof(int));
-	int *moves = (int *)malloc(max_moves*sizeof(int));*/
 
 	int heap_index, user_move, comp_move, comp_move_index, num = 0, win_user = 0;
 	int *comp_opt_move;
@@ -163,7 +280,7 @@ void main()
 
 		/*printf("\nState after User Move : ");
 		for(j=0;j<k;j++)
-			printf("%d ",N[j]);
+			printf("%d ",N[j]%(M[j]+1));
 		printf("\n");*/
 
 		zero_flag = all_zero(N,k);
@@ -186,7 +303,7 @@ void main()
 
 		/*printf("\nState after Comp Move : ");
 		for(j=0;j<k;j++)
-			printf("%d ",N[j]);
+			printf("%d ",N[j]%(M[j]+1));
 		printf("\n");*/
 		
 		/*for(j=0;j<i;j++)
