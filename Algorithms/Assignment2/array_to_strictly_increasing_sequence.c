79a3879614337254b8c08cd1bd1c3b4f07583e04
Siladittya Manna <sadimanna@users.noreply.github.com>
2020-04-13 11:42:29 +0530

    Update array_to_strictly_increasing_sequence.c

--- Algorithms/Assignment2/array_to_strictly_increasing_sequence.c
+++ Algorithms/Assignment2/array_to_strictly_increasing_sequence.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int LIS(int *seq, int arrlen)
+{
+	//FINDING THE LONGEST INCREASING SEQUENCE
+	int *longest_inc_seq = (int *)malloc(arrlen*sizeof(int));
+	int *smallest_elem_of_lis = (int *)malloc(arrlen*sizeof(int));
+	int  prev_lis_se = 0;
+
+	int longest_len = 1;
+	longest_inc_seq[0] = 1;
+	smallest_elem_of_lis[0] = 0;
+	int i = arrlen-1;
+
+	for(i=1;i<arrlen;i++)
+	{
+		longest_inc_seq[i]=1;
+		smallest_elem_of_lis[i] = i;
+		for(int j=0;j<i;j++)
+		{
+			if((seq[i]>seq[j])&&(seq[i]-seq[j])>=(i-j))
+			{
+				if(longest_inc_seq[j]+1 > longest_inc_seq[i])
+				{
+					smallest_elem_of_lis[i] = smallest_elem_of_lis[j];
+					//seolis_ind = j;
+					longest_inc_seq[i] = longest_inc_seq[j]+1;
+				}
+			}
+		}
+		if((longest_len < longest_inc_seq[i])&&(seq[smallest_elem_of_lis[i]]-1>=smallest_elem_of_lis[i]-prev_lis_se))
+		{
+			
+			longest_len = longest_inc_seq[i];
+			prev_lis_se = smallest_elem_of_lis[i];
+		}
+	}
+	return longest_len;
+}
+
 void main()
 {
 	int MAX_NUM = 10;
@@ -9,7 +47,6 @@ void main()
 	char c;
 
 	//READING THE SEQUENCE
-	printf("Input : \n");
 	while(scanf("%c",&c))
 	{
 		//printf("Input read : %d\n",c);
@@ -26,7 +63,6 @@ void main()
 				seq = (int *)realloc(seq,2*MAX_NUM*sizeof(int));
 				MAX_NUM = 2*MAX_NUM;
 			}
-			//printf("Input number : %d; Input : %d\n",num_inps,num);
 			seq[num_inps-1] = num;
 			num = 0;			
 		}
@@ -35,35 +71,16 @@ void main()
 			break;
 	}
 
-	printf("\nSequence : \n");
+	/*printf("Sequence : \n");
 	for(i=0;i<num_inps;i++)
 		printf("%d ",seq[i]);
-	printf("\n");
+	printf("\n");*/
 
-	//FINDING THE LONGEST INCREASING SEQUENCE
-	int *longest_inc_seq = (int *)malloc(num_inps*sizeof(int));
-
-	longest_inc_seq[0] = 1;
-	int longest_len = 1;
-
-	for(i=1;i<num_inps;i++)
-	{
-		longest_inc_seq[i] = 1;
-		longest_len = 1;
-
-		for(j=0;j<i;j++)
-			if((seq[j]<seq[i])&&(longest_inc_seq[j]+1 > longest_len))
-				longest_len = longest_inc_seq[j]+1;
-
-		longest_inc_seq[i] = longest_len;
-	}
-	
-	printf("Length of longest increasing sequence : ");
-	printf("%d\n",longest_len);
+	int longest_len = LIS(seq, num_inps);
 
 	//wE HAVE TO REPLACE THE REMAINING ELEMENTS BY INTERMEDIATE NUMBERS
-	printf("Minimum number of integers that must be changed");
-	printf("to make the input sequence strictly increasing : \n");
+	//printf("Minimum number of integers that must be changed");
+	//printf("to make the input sequence strictly increasing : \n");
 	printf("\n%d\n\n",num_inps-longest_len);
 
 }
